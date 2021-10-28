3bd336f9ce2b01fc47d471a94abdf0374c8510d8
LishuGupta652 <lishugupta652@gmail.com>
2021-10-28 12:33:44 +0000

    node turning

---
+++ ecommerce/node.js
@@ -0,0 +1,16 @@
+console.log("node app");
+
+var a= 10;
+var b = 5;
+
+
+// module
+const os = require('os');
+console.log(os.type())
+console.log(os.release())
+console.log(os.platform())
+
+const car = require('./car.js');
+console.log(car)
+
+
